098c03aefcc184a80d1c706b22e637f24af87acb
protarie125 <protarie@gmail.com>
2018-05-05 02:33:28 +0900

    initial commit

---
+++ Ch02-ex1/Ch02-ex1/Ex1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch02_ex1
+{
+    class Ex1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+            // 콘솔 화면에 문자열 출력
+        }
+    }
+}
